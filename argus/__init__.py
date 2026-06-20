f366005e830b4c3180e75f7ee18d35daf84b2dc6
Minkxx <monsurcodes@gmail.com>
2026-06-20 10:37:59 +0530

    feat: remove main and build_parser

--- argus/__init__.py
+++ argus/__init__.py
@@ -1,12 +1,6 @@
 from argus.audit import CodebaseAuditor
-from argus.cli import build_parser, main
 from argus.engines.ollama_engine import OllamaEngine
 from argus.strategies.nestjs_app import NestJsStrategy
 
-__all__ = [
-    "CodebaseAuditor",
-    "OllamaEngine",
-    "NestJsStrategy",
-    "build_parser",
-    "main",
-]
+
+__all__ = ["CodebaseAuditor", "OllamaEngine", "NestJsStrategy"]
